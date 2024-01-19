083ba1d7ae41417a8c3eb8ada72edde2997b67e3
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2024-01-19 15:11:51 -0600

    Primer commit

---
+++ promesa.js
@@ -0,0 +1,16 @@
+﻿//1
+//Las promesas son objetos que representan la terminación o fracaso eventual de una operación o función asincrona
+let promesaExitosa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promesa exitosa")
+    }, 3000);
+});
+
+//then: se ejecuta cuando la promesa se resulve exitosamente
+//catch: se ejecuta cuando la promesa es rechazada
+promesaExitosa.then((mensaje) => {
+    console.log(mensaje);
+}).catch((error) => {
+    console.error(error);
+})
+
