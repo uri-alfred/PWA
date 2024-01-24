77327d1d178bb4200db90a6677ee83c32aecf791
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2024-01-24 17:55:12 -0600

    fetch8

--- promesa.js
+++ promesa.js
@@ -1,16 +1,10 @@
-﻿//fetch 7
+﻿//fetch 8
 
-// https://fakestoreapi.com/products/1
-
-fetch('https://pokeapi.co/api/v2/pokemon-form/100000')
-    .then(resp => {
-        if (resp.ok) {
-            return resp.json()
-        } else {
-            throw new Error('No existe producto')
-        }
+fetch('not-found.html')
+    .then(resp => resp.text())
+    .then(html => {
+        document.querySelector('body').innerHTML = html
     })
-    .then(console.log)
     .catch(err => {
         console.log('Error en la petición')
         console.log(err)
