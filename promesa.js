d58b3d1e67e3980d052e74ac7c5fe749ed6bd9fd
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2024-01-26 12:58:09 -0600

    fetch6

--- promesa.js
+++ promesa.js
@@ -1,24 +1,5 @@
-﻿//fetch 5
+﻿//fetch 6
 
-//Fetch archivos locales
-
-//Almacena de manera local una imagen dentro de este
-//proyecto en la carpeta img realiza una solicitud fech de esta imagen
-//y muestrala en el navegador
-
-fetch("./img/archangel.png")
-    .then(resp => {
-        console.log(resp);
-        return resp.url
-    })
-    .then(img => {
-        console.log(img)
-        let imagen = document.createElement("img");
-        imagen.src = img;
-        imagen.style = "width: 600px"
-        document.body.appendChild(imagen);
-    })
-    .catch(error => {
-        console.log('Error en la solicitud de la imagen')
-        console.log(error)
-    })
+fetch('https://fakestoreapi.com/products/1')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
